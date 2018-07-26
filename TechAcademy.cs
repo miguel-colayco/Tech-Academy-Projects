78e4280209b76beceb0230cb48fd92dd750b0418
miguel-colayco <40243891+miguel-colayco@users.noreply.github.com>
2018-07-26 13:06:05 -0700

    Add files via upload
    Second C# program

---
+++ TechAcademy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+		Console.WriteLine("What course are you on?");
+		string course = Console.ReadLine();
+		Console.WriteLine("What page number ?");
+        string pageNumer = Console.ReadLine();
+		Console.WriteLine("Do you need help with anything? Please answer true or false");
+		string needHelp = Console.ReadLine();
+		Console.WriteLine("Is there any other feedback you'd like to provide?  Please be specific");
+		Console.ReadLine();
+		Console.WriteLine("How many hours did you study today? ");
+		string numberHours = Console.ReadLine();
+		Console.WriteLine("Thank you for your answers.  An Instructor will respond to this shortly.  Have a great day!");
+		
+    }
+    }
+
