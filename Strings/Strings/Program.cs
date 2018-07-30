627d1bee790014c85dd1e87a8e1341c71381da00
miguel-colayco <40243891+miguel-colayco@users.noreply.github.com>
2018-07-30 16:54:18 -0700

    Add files via upload
    String Exercise

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "Hi ";
+            string second = "My name is ";
+            string third = "Mike";
+            string together =  first + second + third;
+            Console.WriteLine(together);
+            Console.WriteLine("Upper Case: " + together.ToUpper());
+            StringBuilder paragraph = new StringBuilder();
+
+                string sentence = Console.ReadLine();
+                paragraph.Append(sentence);
+           
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+        }
+    }
+}
