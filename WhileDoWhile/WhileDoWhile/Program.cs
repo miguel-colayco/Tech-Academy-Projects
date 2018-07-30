5f2c66c261ef3d357606fe1181a01a304984b1a9
miguel-colayco <40243891+miguel-colayco@users.noreply.github.com>
2018-07-30 11:03:46 -0700

    Add files via upload
    While and Do While

---
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Int32 number = 0;
+            // while (number < 10) {
+            //     Console.WriteLine("Give me a number ");
+            //     number = Convert.ToInt32(Console.ReadLine());
+            //  }
+            // Console.WriteLine("Greater than 10, I'm done! ");
+            // Console.ReadLine();
+            Int32 number = 20;
+            do {
+                Console.WriteLine("Give me a number ");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            while (number < 10 );
+            Console.WriteLine("Greater than 10, I'm done! ");
+            Console.ReadLine();
+        }
+    }
+}
