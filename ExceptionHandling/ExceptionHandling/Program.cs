c73055b54a68628d411faba3eb68d5c06f781a2e
miguel-colayco <40243891+miguel-colayco@users.noreply.github.com>
2018-07-31 15:54:40 -0700

    Add files via upload
    Exception Handling exercise

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+        List<int> numbers = new List<int>() {12,10,6,8,14,18};
+        try
+        {
+            
+            Console.WriteLine("Pick a number.");
+            int divideBy = Convert.ToInt32(Console.ReadLine());
+            foreach (int number in numbers) {
+                int quotient = number / divideBy;
+                Console.WriteLine(quotient);
+            }
+
+        }
+        catch (FormatException )
+        {
+            Console.WriteLine("Please type a whole number ");
+            return;
+        }
+        catch (DivideByZeroException )
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+        Console.ReadLine();
+        }
+    }
+
